a76561ad88cd4f0e551768a356d8e8ae23fda0cb
Superfluxus <38507824+Superfluxus@users.noreply.github.com>
2019-03-06 11:29:21 +0000

    Add files via upload

---
+++ Win10 key.ps1
@@ -0,0 +1,3 @@
+﻿$key = wmic path softwarelicensingservice get OA3xOriginalProductKey
+cscript //NoLogo //B %windir%\system32\slmgr.vbs /ipk $key 
+cscript //NoLogo //B %windir%\system32\slmgr.vbs /ato
